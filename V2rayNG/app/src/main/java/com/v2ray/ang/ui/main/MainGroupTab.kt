a873d21d532db9c12337d86fcdc157e91bd1edf5
2dust <31833384+2dust@users.noreply.github.com>
2026-08-30 17:44:53 +0800

    Fix
    https://github.com/2dust/v2rayNG/issues/6153
    (cherry picked from commit 3dc0173b9fe0ed814d8bd5be06d16d74be6a4858)

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/main/MainGroupTab.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/main/MainGroupTab.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.v2ray.ang.R
 import com.v2ray.ang.dto.GroupMapItem
